4d4c9c90d9349ae459e853722b8dbbdac70854b6
literate <gaekoon@gmail.com>
2022-06-12 21:33:17 +0900

    api 호출을 비동기 및 커스텀 훅으로 변경

--- src/Users.js
+++ src/Users.js
@@ -1,46 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { useAsync } from "./useAsync";
+import User from "./User";
 
-function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const fetchUsers = async () => {
-    try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
+const getUsers = async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return response.data;
+};
 
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      setUsers(response.data);
-    } catch (e) {
-      console.log(e.response.status);
-      setError(e);
-    }
-    setLoading(false);
-  };
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+export const Users = () => {
+  const [state, refetch] = useAsync(getUsers, [], true);
+  const [userId, setUserId] = useState(null);
+  const { loading, data: users, error } = state;
   if (loading) return <div>로딩 중....</div>;
   if (error) return <div>에러 발생</div>;
-  if (!users) return null;
-
+  if (!users) return <button onClick={refetch}>불러오기</button>;
   return (
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 요청하기</button>
+      <button onClick={refetch}>다시 요청하기</button>
+      {userId && <User id={userId} />}
     </>
   );
-}
-
-export default Users;
+};
